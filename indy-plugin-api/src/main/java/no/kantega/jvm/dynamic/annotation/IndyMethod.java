e29107fa2c3b58d47571885cb2094f5866696c01
marska <martin.skarsaune@kantega.no>
2016-09-11 22:06:51 +0200

    better documentation of annotation

--- indy-plugin-api/src/main/java/no/kantega/jvm/dynamic/annotation/IndyMethod.java
+++ indy-plugin-api/src/main/java/no/kantega/jvm/dynamic/annotation/IndyMethod.java
@@ -6,18 +6,25 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Retention(RetentionPolicy.RUNTIME)
-@Target(value={ METHOD})
+/**
+ * I enable rerouting calls to method annotated by me
+ * will use invoke dynamic to a bootstrap method that in turn may return an implementation
+ */
+@Retention(RetentionPolicy.SOURCE)
+@Target(value = { METHOD })
 public @interface IndyMethod {
-	
+
 	/**
 	 * 
-	 * @return Fully qualified name of the Java class containing the actual implementation
+	 * @return Fully qualified name of the Java class containing the bootstrap method
 	 */
 	String implementation();
+
 	/**
 	 * 
-	 * @return Name of a method that complies with the requirements
+	 * @return Name of boostrap method that complies with the requirements public
+	 *         static method(MethodHandles.Lookup, String, MethodType) that in
+	 *         turn provides reference to a concrete method
 	 */
 	String method();
 
